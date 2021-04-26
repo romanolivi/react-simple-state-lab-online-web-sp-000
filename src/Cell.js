e55f64352a6323245044134c83560eda1bb80c32
romanolivi <romano_livi@outlook.com>
2021-04-26 18:35:15 -0400

    commit tree 403e2ecf55c0055ee8e5fbc4ce0b55eeae19efb3

---
+++ src/Cell.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: this.props.value
+        }
+    }
+
+    render() {
+        return (
+            <div className="cell" style={{backgroundColor: this.state.color}} onClick={() => this.setState({color: '#333'})}>
+
+            </div>
+        )
+    }
+}
+
